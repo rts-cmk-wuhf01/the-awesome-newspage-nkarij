a57817df9e82d20b997b5cd834401ed0f1aa1ca8
Nanna <5608562@RTS-365.dk>
2019-05-28 15:14:16 +0200

    update awesome newspage

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,35 +2,35 @@ module.exports = (app) => {
 
    app.get('/', (req, res, next) => {
       res.render('home', {
-         "title" : "Forside",
+         "title" : "The News Paper - News & Lifestyle Magazine Template",
          "page" : "Home"
       });
    });
 
    app.get('/contact', (req, res, next) => {
       res.render('contact', {
-         "title" : "Kontakt Os",
+         "title" : "The News Paper - News & Lifestyle Magazine Template",
          "page" : "Contact"
       });
    });
 
    app.get('/about', (req, res, next) => {
       res.render('about', {
-         "title" : "Om os",
+         "title" : "The News Paper - News & Lifestyle Magazine Template",
          "page" : "About"
       });
    });
 
    app.get('/categories-post', (req, res, next) => {
       res.render('categories-post', {
-         "title" : "Kategorier",
+         "title" : "The News Paper - News & Lifestyle Magazine Template",
          "page" : "Categories-post"
       });
    });
 
    app.get('/single-post', (req, res, next) => {
       res.render('single-post', {
-         "title" : "Post",
+         "title" : "The News Paper - News & Lifestyle Magazine Template",
          "page" : "Single-post"
       });
    });
