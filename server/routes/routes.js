acf6c51085a56dace02a78f3f03df7c8dfd1a76b
Nanna <5608562@RTS-365.dk>
2019-06-03 12:41:32 +0200

    home date-and-time done

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,83 @@
 module.exports = (app) => {
 
+   let data = { 
+      "news" : [
+            {
+               "category" : "Finance",
+               "title" : "Financial news: A new company is born today at the stock market",
+               "datetime" : new Date('2019-01-14 07:00:14')
+            }, 
+            {
+               "category" : "Politics",
+               "title" : "Sed a elit euismod augue semper congue sit amet ac sapien.",
+               "datetime" : new Date('2019-02-13 07:00:14')
+            },
+            {
+               "category" : "Health",
+               "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", 
+               "datetime" : new Date('2019-03-12 07:00:14')
+            },
+            {
+               "category" : "Finance",
+               "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",  
+               "datetime" : new Date('2019-04-11 07:00:14')
+            },
+            {
+               "category" : "Travel",
+               "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", 
+               "datetime" : new Date('2019-05-10 07:00:14')
+            },
+            {
+               "category" : "Politics",
+               "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.", 
+               "datetime" : new Date('2019-06-09 07:00:14')
+            }
+         ],
+      "popularnews" : [
+         {
+            "rank" : "1",
+            "title" : "Financial news: A new company is born today at the stock market",
+            "datetime" : new Date('2019-01-14 07:00:14')
+         }, 
+         {
+            "rank" : "2",
+            "title" : "Sed a elit euismod augue semper congue sit amet ac sapien.",
+            "datetime" : new Date('2019-02-13 07:00:14')
+         },
+         {
+            "rank" : "3",
+            "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", "datetime" : new Date('2019-03-12 07:00:14')
+         },
+         {
+            "rank" : "4",
+            "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",  
+            "datetime" : new Date('2019-04-11 07:00:14')
+         }
+      ],
+   "latestcomments" : [
+      {
+         "name" : "Jamie Smith",
+         "title" : "Facebook is offering facial recognition.",
+         "datetime" : new Date('2019-01-14 07:00:14')
+      }, 
+      {
+         "name" : "Jamie Smith",
+         "title" : "Facebook is offering facial recognition.",
+         "datetime" : new Date('2019-02-13 07:00:14')
+      },
+      {
+         "name" : "Jamie Smith",
+         "title" : "Facebook is offering facial recognition.",
+         "datetime" : new Date('2019-03-12 07:00:14')
+      },
+      {
+         "name" : "Jamie Smith",
+         "title" : "Facebook is offering facial recognition.",
+         "datetime" : new Date('2019-04-11 07:00:14')
+      }
+   ]
+} // data ended
+
    app.get('/', (req, res, next) => {
       
       let products = [
@@ -18,7 +96,8 @@ module.exports = (app) => {
       res.render('home', {
          "allProducts" : products,
          "title" : "The News Paper - News & Lifestyle Magazine Template",
-         "page" : "Home"
+         "page" : "Home",
+         "data" : data
       });
    });
 
@@ -42,85 +121,6 @@ module.exports = (app) => {
       // let date1 = app.locals.dateAndTime.parse('2015/01/02 23:14:05', 'YYYY/MM/DD HH:mm:ss');
       // let formattedDate = app.locals.dateAndTime.format(date1, 'YYYY/MM/DD HH:mm:ss');
       // console.log(formattedDate);
-      let data = { 
-         "news" : [
-               {
-                  "category" : "Finance",
-                  "title" : "Financial news: A new company is born today at the stock market",
-                  "datetime" : new Date('2019-01-14 07:00:14')
-               }, 
-               {
-                  "category" : "Politics",
-                  "title" : "Sed a elit euismod augue semper congue sit amet ac sapien.",
-                  "datetime" : new Date('2019-02-13 07:00:14')
-               },
-               {
-                  "category" : "Health",
-                  "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", 
-                  "datetime" : new Date('2019-03-12 07:00:14')
-               },
-               {
-                  "category" : "Finance",
-                  "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",  
-                  "datetime" : new Date('2019-04-11 07:00:14')
-               },
-               {
-                  "category" : "Travel",
-                  "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", 
-                  "datetime" : new Date('2019-05-10 07:00:14')
-               },
-               {
-                  "category" : "Politics",
-                  "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.", 
-                  "datetime" : new Date('2019-06-09 07:00:14')
-               }
-            ],
-         "popularnews" : [
-            {
-               "rank" : "1",
-               "title" : "Financial news: A new company is born today at the stock market",
-               "datetime" : new Date('2019-01-14 07:00:14')
-            }, 
-            {
-               "rank" : "2",
-               "title" : "Sed a elit euismod augue semper congue sit amet ac sapien.",
-               "datetime" : new Date('2019-02-13 07:00:14')
-            },
-            {
-               "rank" : "3",
-               "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", "datetime" : new Date('2019-03-12 07:00:14')
-            },
-            {
-               "rank" : "4",
-               "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",  
-               "datetime" : new Date('2019-04-11 07:00:14')
-            }
-         ],
-      "latestcomments" : [
-         {
-            "name" : "Jamie Smith",
-            "title" : "Facebook is offering facial recognition.",
-            "datetime" : new Date('2019-01-14 07:00:14')
-         }, 
-         {
-            "name" : "Jamie Smith",
-            "title" : "Facebook is offering facial recognition.",
-            "datetime" : new Date('2019-02-13 07:00:14')
-         },
-         {
-            "name" : "Jamie Smith",
-            "title" : "Facebook is offering facial recognition.",
-            "datetime" : new Date('2019-03-12 07:00:14')
-         },
-         {
-            "name" : "Jamie Smith",
-            "title" : "Facebook is offering facial recognition.",
-            "datetime" : new Date('2019-04-11 07:00:14')
-         }
-      ]
-   } // data ended
-
-
       res.render('categories-post', {
          "title" : "The News Paper - News & Lifestyle Magazine Template",
          "page" : "Categories-post",
