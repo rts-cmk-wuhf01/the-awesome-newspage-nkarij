8777560995fa0a4aa863968508b9ec55f88fe880
Nanna <5608562@RTS-365.dk>
2019-06-04 15:05:13 +0200

    New Homepage ( problemer m den originale struktur)

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,61 +1,88 @@
 module.exports = (app) => {
 
    let data = {
-      // NB JEG BRUGER SAMME DATGA TIL BÅDE HOME OG CATEGORIES, MEN DER ER FLERE
-      // ARTIKLER I CATEGORIES END I HOME. MEN NU ER DETTE BARE ET EKSEMPEL.
-      "news" : [
-            {
-               "category" : "Finance",
-               "title" : "Financial news: A new company is born today at the stock market",
-               "datetime" : new Date('2019-01-14 07:00:14')
-            }, 
-            {
-               "category" : "Politics",
-               "title" : "Sed a elit euismod augue semper congue sit amet ac sapien.",
-               "datetime" : new Date('2019-02-13 07:00:14')
-            },
-            {
-               "category" : "Health",
-               "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", 
-               "datetime" : new Date('2019-03-12 07:00:14')
-            },
-            {
-               "category" : "Finance",
-               "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",  
-               "datetime" : new Date('2019-04-11 07:00:14')
-            },
-            {
-               "category" : "Travel",
-               "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", 
-               "datetime" : new Date('2019-05-10 07:00:14')
-            },
-            {
-               "category" : "Politics",
-               "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.", 
-               "datetime" : new Date('2019-06-09 07:00:14')
-            }
-         ],
-      "popularnews" : [
-         {
-            "rank" : "1",
-            "title" : "Financial news: A new company is born today at the stock market",
-            "datetime" : new Date('2019-01-14 07:00:14')
-         }, 
-         {
-            "rank" : "2",
-            "title" : "Sed a elit euismod augue semper congue sit amet ac sapien.",
-            "datetime" : new Date('2019-02-13 07:00:14')
-         },
-         {
-            "rank" : "3",
-            "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", "datetime" : new Date('2019-03-12 07:00:14')
-         },
-         {
-            "rank" : "4",
-            "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",  
-            "datetime" : new Date('2019-04-11 07:00:14')
-         }
-      ],
+      // NB SÆT FLERE DATASÆT IND I NEWS
+   "featured2" : [
+      {
+         "category" : "Finance",
+         "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placerat. Sed varius leo ac..."
+      },
+      {
+         "category" : "Finance",
+         "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placerat. Sed varius leo ac..."
+      },     
+   ],
+   "relatednews" : [
+      {  
+         "category" : "Finance",
+         "text" : "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac..."
+      },
+      {  
+         "category" : "Finance",
+         "text" : "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac..."
+      },
+      {  
+         "category" : "Finance",
+         "text" : "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac..."
+      },
+      {  
+         "category" : "Finance",
+         "text" : "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac..."
+      },
+   ],
+   "news" : [
+      {
+         "category" : "Finance",
+         "title" : "Financial news: A new company is born today at the stock market",
+         "datetime" : new Date('2019-01-14 07:00:14')
+      }, 
+      {
+         "category" : "Politics",
+         "title" : "Sed a elit euismod augue semper congue sit amet ac sapien.",
+         "datetime" : new Date('2019-02-13 07:00:14')
+      },
+      {
+         "category" : "Health",
+         "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", 
+         "datetime" : new Date('2019-03-12 07:00:14')
+      },
+      {
+         "category" : "Finance",
+         "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",
+         "datetime" : new Date('2019-04-11 07:00:14')
+      },
+      {
+         "category" : "Travel",
+         "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", 
+         "datetime" : new Date('2019-05-10 07:00:14')
+      },
+      {
+         "category" : "Politics",
+         "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",
+         "datetime" : new Date('2019-06-09 07:00:14')
+      }
+   ],
+   "popularnews" : [
+      {
+         "rank" : "1",
+         "title" : "Financial news: A new company is born today at the stock market",
+         "datetime" : new Date('2019-01-14 07:00:14')
+      }, 
+      {
+         "rank" : "2",
+         "title" : "Sed a elit euismod augue semper congue sit amet ac sapien.",
+         "datetime" : new Date('2019-02-13 07:00:14')
+      },
+      {
+         "rank" : "3",
+         "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", "datetime" : new Date('2019-03-12 07:00:14')
+      },
+      {
+         "rank" : "4",
+         "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",  
+         "datetime" : new Date('2019-04-11 07:00:14')
+      }
+   ],
    "latestcomments" : [
       {
          "name" : "Jamie Smith",
@@ -120,9 +147,6 @@ module.exports = (app) => {
    app.get('/categories-post', (req, res, next) => {
       // HER I SERVERFILEN ELLER APP.JS SKAL MAN ALTID BRUGE APP.LOCALS TIL  
       // DATEANDTIME MODULET. DET SKAL MAN IKKE I SIN EJS
-      // let date1 = app.locals.dateAndTime.parse('2015/01/02 23:14:05', 'YYYY/MM/DD HH:mm:ss');
-      // let formattedDate = app.locals.dateAndTime.format(date1, 'YYYY/MM/DD HH:mm:ss');
-      // console.log(formattedDate);
       res.render('categories-post', {
          "title" : "The News Paper - News & Lifestyle Magazine Template",
          "page" : "Categories-post",
@@ -132,9 +156,11 @@ module.exports = (app) => {
    });
 
    app.get('/single-post', (req, res, next) => {
+
       res.render('single-post', {
          "title" : "The News Paper - News & Lifestyle Magazine Template",
-         "page" : "Single-post"
+         "page" : "Single-post",
+         "data" : data
       });
    });
 
