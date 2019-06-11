b3a5a1b3b4a935d62ef5de818cfd51619fc59e8e
Nanna <5608562@RTS-365.dk>
2019-06-11 10:48:44 +0200

    pågbegynder dynamiske routes. Dvs. denne version indeholder kun statiske routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -36,112 +36,112 @@ module.exports = (app) => {
       });
    });
 
-   let data = {
-      // NB SÆT FLERE DATASÆT IND I NEWS
-   "featured2" : [
-      {
-         "category" : "Finance",
-         "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placerat. Sed varius leo ac..."
-      },
-      {
-         "category" : "Finance",
-         "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placerat. Sed varius leo ac..."
-      },     
-   ],
-   "relatednews" : [
-      {  
-         "category" : "Finance",
-         "text" : "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac..."
-      },
-      {  
-         "category" : "Finance",
-         "text" : "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac..."
-      },
-      {  
-         "category" : "Finance",
-         "text" : "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac..."
-      },
-      {  
-         "category" : "Finance",
-         "text" : "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac..."
-      },
-   ],
-   "news" : [
-      {
-         "category" : "Finance",
-         "title" : "Financial news: A new company is born today at the stock market",
-         "datetime" : new Date('2019-01-14 07:00:14')
-      }, 
-      {
-         "category" : "Politics",
-         "title" : "Sed a elit euismod augue semper congue sit amet ac sapien.",
-         "datetime" : new Date('2019-02-13 07:00:14')
-      },
-      {
-         "category" : "Health",
-         "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", 
-         "datetime" : new Date('2019-03-12 07:00:14')
-      },
-      {
-         "category" : "Finance",
-         "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",
-         "datetime" : new Date('2019-04-11 07:00:14')
-      },
-      {
-         "category" : "Travel",
-         "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", 
-         "datetime" : new Date('2019-05-10 07:00:14')
-      },
-      {
-         "category" : "Politics",
-         "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",
-         "datetime" : new Date('2019-06-09 07:00:14')
-      }
-   ],
-   "popularnews" : [
-      {
-         "rank" : "1",
-         "title" : "Financial news: A new company is born today at the stock market",
-         "datetime" : new Date('2019-01-14 07:00:14')
-      }, 
-      {
-         "rank" : "2",
-         "title" : "Sed a elit euismod augue semper congue sit amet ac sapien.",
-         "datetime" : new Date('2019-02-13 07:00:14')
-      },
-      {
-         "rank" : "3",
-         "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", "datetime" : new Date('2019-03-12 07:00:14')
-      },
-      {
-         "rank" : "4",
-         "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",  
-         "datetime" : new Date('2019-04-11 07:00:14')
-      }
-   ],
-   "latestcomments" : [
-      {
-         "name" : "Jamie Smith",
-         "title" : "Facebook is offering facial recognition.",
-         "datetime" : new Date('2019-01-14 07:00:14')
-      }, 
-      {
-         "name" : "Jamie Smith",
-         "title" : "Facebook is offering facial recognition.",
-         "datetime" : new Date('2019-02-13 07:00:14')
-      },
-      {
-         "name" : "Jamie Smith",
-         "title" : "Facebook is offering facial recognition.",
-         "datetime" : new Date('2019-03-12 07:00:14')
-      },
-      {
-         "name" : "Jamie Smith",
-         "title" : "Facebook is offering facial recognition.",
-         "datetime" : new Date('2019-04-11 07:00:14')
-      }
-   ]
-} // data ended
+//    let data = {
+//       // NB SÆT FLERE DATASÆT IND I NEWS
+//    "featured2" : [
+//       {
+//          "category" : "Finance",
+//          "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placerat. Sed varius leo ac..."
+//       },
+//       {
+//          "category" : "Finance",
+//          "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placerat. Sed varius leo ac..."
+//       },     
+//    ],
+//    "relatednews" : [
+//       {  
+//          "category" : "Finance",
+//          "text" : "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac..."
+//       },
+//       {  
+//          "category" : "Finance",
+//          "text" : "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac..."
+//       },
+//       {  
+//          "category" : "Finance",
+//          "text" : "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac..."
+//       },
+//       {  
+//          "category" : "Finance",
+//          "text" : "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac..."
+//       },
+//    ],
+//    "news" : [
+//       {
+//          "category" : "Finance",
+//          "title" : "Financial news: A new company is born today at the stock market",
+//          "datetime" : new Date('2019-01-14 07:00:14')
+//       }, 
+//       {
+//          "category" : "Politics",
+//          "title" : "Sed a elit euismod augue semper congue sit amet ac sapien.",
+//          "datetime" : new Date('2019-02-13 07:00:14')
+//       },
+//       {
+//          "category" : "Health",
+//          "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", 
+//          "datetime" : new Date('2019-03-12 07:00:14')
+//       },
+//       {
+//          "category" : "Finance",
+//          "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",
+//          "datetime" : new Date('2019-04-11 07:00:14')
+//       },
+//       {
+//          "category" : "Travel",
+//          "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", 
+//          "datetime" : new Date('2019-05-10 07:00:14')
+//       },
+//       {
+//          "category" : "Politics",
+//          "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",
+//          "datetime" : new Date('2019-06-09 07:00:14')
+//       }
+//    ],
+//    "popularnews" : [
+//       {
+//          "rank" : "1",
+//          "title" : "Financial news: A new company is born today at the stock market",
+//          "datetime" : new Date('2019-01-14 07:00:14')
+//       }, 
+//       {
+//          "rank" : "2",
+//          "title" : "Sed a elit euismod augue semper congue sit amet ac sapien.",
+//          "datetime" : new Date('2019-02-13 07:00:14')
+//       },
+//       {
+//          "rank" : "3",
+//          "title" : "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.", "datetime" : new Date('2019-03-12 07:00:14')
+//       },
+//       {
+//          "rank" : "4",
+//          "title" : "Augue semper congue sit amet ac sapien. Fusce consequat.",  
+//          "datetime" : new Date('2019-04-11 07:00:14')
+//       }
+//    ],
+//    "latestcomments" : [
+//       {
+//          "name" : "Jamie Smith",
+//          "title" : "Facebook is offering facial recognition.",
+//          "datetime" : new Date('2019-01-14 07:00:14')
+//       }, 
+//       {
+//          "name" : "Jamie Smith",
+//          "title" : "Facebook is offering facial recognition.",
+//          "datetime" : new Date('2019-02-13 07:00:14')
+//       },
+//       {
+//          "name" : "Jamie Smith",
+//          "title" : "Facebook is offering facial recognition.",
+//          "datetime" : new Date('2019-03-12 07:00:14')
+//       },
+//       {
+//          "name" : "Jamie Smith",
+//          "title" : "Facebook is offering facial recognition.",
+//          "datetime" : new Date('2019-04-11 07:00:14')
+//       }
+//    ]
+// } // data ended
 
    app.get('/', (req, res, next) => {
       res.render('home', {
@@ -165,13 +165,29 @@ module.exports = (app) => {
       });
    });
 
-   app.get('/categories-post', (req, res, next) => {
-      // HER I SERVERFILEN ELLER APP.JS SKAL MAN ALTID BRUGE APP.LOCALS TIL  
-      // DATEANDTIME MODULET. DET SKAL MAN IKKE I SIN EJS
+   // her tilføjes et endpoint til siden vha /:
+   // fx: /categories-post/3. Her er 3 = parameter værdien, 
+   // som er tilgængelig i route koden via req.params.category_id
+   // app.get('/categories-post/:category_id', async (req, res, next) => {
+   //    res.send(req.params.category_id); // for demonstrationens skyld! 
+   //    // her kan alle kategoriens artikler hentes osv...
+   // });
+
+   app.get('/categories-post/:category_id', (req, res, next) => {
+
+      let database = await mysql.connect();
+
+      // NB BEMÆRK BACKTICKS FOR LINJESKIFT, sådan får man fat i de relaterede tabeller:
+      let [productsandcategories] = await database.execute(`
+         SELECT * FROM news
+         INNER JOIN newscategories ON fk_newscategoryID = newsCategoryID
+      `);
+      database.end();
+
       res.render('categories-post', {
-         "title" : "The News Paper - News & Lifestyle Magazine Template",
-         "page" : "Categories-post",
-         "data" : data
+         "categories" : categories,
+         // index 0, fordi man altid vil hente via 1 id. og der er altid kun 1 id.
+         "category" : categories[0]
       });
 
    });
